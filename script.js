ecb19e2c06ac41d713fba90585623b7ca6b52f29
Hanson Sun <65196939+Hanson-Sun@users.noreply.github.com>
2021-10-16 21:41:19 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,338 @@
+
+greet = `
+  _  _                          ___           
+ | || |__ _ _ _  ___ ___ _ _   \/ __|_  _ _ _  
+ | __ \/ _\` | ' \\(_-</ _ \\ ' \\  \\__ \\ || | ' \\ 
+ |_||_\\__,_|_||_\/__\/\\___\/_||_| |___\/\\_,_|_||_|     
+
+
+.....................................................................
+ Welcome to my terminal, enter "help" for the documentation of all commands. 
+ 
+`
+
+help = `
++=============+===============================================+
+|  Function   |                      Use                      |
++=============+===============================================+
+| help        | displays a list of all the commands           |
++-------------+-----------------------------------------------+
+| about       | A brief introduction of who I am              |
++-------------+-----------------------------------------------+
+| skills      | My skills and abilities                       |
++-------------+-----------------------------------------------+
+| experience  | my experiences and what I've accomplished     |
++-------------+-----------------------------------------------+
+| projects    | a showcase of some of my programming projects |
++-------------+-----------------------------------------------+
+| awards      | my acheivements recognized by others          |
++-------------+-----------------------------------------------+
+| education   | a summary of my education journey             |
++-------------+-----------------------------------------------+
+| contact     | my contact info                               |
++-------------+-----------------------------------------------+
+| download_cv | download a pdf version of my CV               |
++-------------+-----------------------------------------------+
+| all         | a concatenated list of everything             |
++-------------+-----------------------------------------------+
+| clear       | clears terminal                               |
++-------------+-----------------------------------------------+
+|             | may have some easter eggs                     |
++-------------+-----------------------------------------------+
+`
+about = `
+Hello, my name is Hanson Sun, a grade 12 IB student from Richmond Secondary School. I am a firm believer of the importance of technology and education in future society. Moreover, I enjoy investigating the beauty of the natural world through mathematics and programmatic simulations.  
+<br>
+<br>
+Hobbies:
+	<ul class = "awards">
+		<li>Programming</li>
+		<li>Cycling</li>
+		<li>Playing the saxophone</li>
+		<li>Arduino Robotics</li>
+		<li>Sleep enthusiast</li>
+		<li>Amature food critic (with very low standards)</li>
+	</ul>
+`
+skills = `
+Programming
+
+	<table class = 'skills'>
+	<tr>
+		<td>+ HTML,CSS</td>
+		<td>▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░</td>
+	</tr>
+	<tr>
+		<td>+ Javascript</td>
+		<td>▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░ </td>
+	</tr>
+	<tr>
+		<td>+ Python</td>
+		<td>▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░</td>
+	</tr>
+	<tr>
+		<td>+ Julia</td>
+		<td>▓▓▓▓▓▓▓▓▓▓▓▓░░░░░░░░</td>
+	</tr>
+	<tr>
+		<td>+ C++</td>
+		<td>▓▓▓▓▓▓▓▓░░░░░░░░░░░░ </td>
+	</tr>
+	</table>
+Academics
+
+	<table class = 'skills'>
+	<tr>
+		<td>+ Mathematics</td>
+		<td>▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░</td>
+	</tr>
+	<tr>
+		<td>+ Physics</td>
+		<td>▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░ </td>
+	</tr>
+	<tr>
+		<td>+ Chemistry</td>
+		<td>▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░░</td>
+	</tr>
+	<tr>
+		<td>+ English</td>
+		<td>▓▓▓▓▓▓▓▓▓▓▓▓░░░░░░░░</td>
+	</tr>
+
+	</table>
+General
+
+	<table class = 'skills'>
+	<tr>
+		<td>+ Chinese</td>
+		<td>▓▓▓▓▓▓▓▓▓▓░░░░░░░░░░</td>
+	</tr>
+	<tr>
+		<td>+ Communication</td>
+		<td>▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░</td>
+	</tr>
+	<tr>
+		<td>+ Leadership</td>
+		<td>▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░░░</td>
+	</tr>
+	<tr>
+		<td>+ Teamwork</td>
+		<td>▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░</td>
+	</tr>
+
+	</table>
+`
+education = `
+	<ul class = "awards">
+		<li>Richmond Secondary School IB Diploma Student (hopefully grad soon)</li>
+		<li>More to come!</li>
+	</ul>
+
+`
+experience = `
+	<ul class = "awards">
+		<li>Co-founder of a non-profit math tutoring organization, Math Connect.</li>
+		<li>Co-founder of the RHS Programming Club</li>
+		<li>Executive memember of the RHS Math Club</li>
+		<li>Member of the RHS Science Team</li>
+	</ul>
+`
+
+awards = `
+	<ul class = "awards">
+		<li>1st place in district for 2021 Waterloo's Sir Isaac Newton Physics Exam</li>
+		<li>82nd place in Canada for 2021 Waterloo's Avogadro Contest</li>
+		<li>9th place during 2021 UBC Physics Olympics, placing 3rd for the Speed of Sound Event (the event I was mainly responible for)</li>
+		<li>Provincal placement for 2019 Math Challengers Competetion</li>
+		<li>3rd place during the 2019 Kwantlen Science Competetion</li>
+		<li>4th place during the 2018 Kwantlen Science Competetion</li>
+		<li>Principal's Award throughout highschool</li>
+		<li>Mathematics HL 11 Award, Chemistry SL 11 Award, Physics 11 Award, Web Development 9/10 Award, English 10 Award. </li>
+	</ul>
+`
+
+projects = `
+
+A list of notable cool things I've made, some for fun, some a bit less fun
+<br><br>
+<table>
+	<tr>
+		Formal Analysis of Constrained Dynamic Systems with mathematics and programming
+		<ul>
+			<li>A formal mathematics paper written for the IB Program that analyzes constrained systems. </li>
+			<li><a target=”_blank” href = "HansonS_Math_EE.pdf">PDF Link</a></li>
+			<li><a target=”_blank” href = "https://hanson-sun.github.io/Math-EE/">Live Demo</a></li>
+		</ul>
+	</tr>
+	<tr>
+		Informal Analysis of Evolutionary Genetic Algorithms in Javascript
+		<ul>
+			<li>A fun investigation of genetic algorithms that I decided to write for the Programming Club.</li>
+			<li><a target=”_blank” href="https://evolutionary-genetic-algorithms.hansonsun.repl.co">Demo</a>
+			</li>
+		</ul>
+	</tr>	
+	<tr>
+		My journey of programmatic physics
+		<ul>
+			<li>A big passion of mine, also influenced my formal analysis of constrained dynamics</li>
+			<li><a target=”_blank” href = "https://bounce.hansonsun.repl.co">Projectile Motion</a></li>
+			<li><a target=”_blank” href = "https://Charge-Simulation.hansonsun.repl.co">Charge Simulation</a></li>
+			<li><a target=”_blank” href = "https://Gravity-Simulation.hansonsun.repl.co">Gravity Simulation</a></li>
+			<li><a target=”_blank” href = "https://metaballs.hansonsun.repl.co">Isosurfaces</a></li>
+			<li><a target=”_blank” href = "https://sad-cloth-simulation.hansonsun.repl.co/hmmm.html">Cloth Simulation</a></li>
+			<li><a target=”_blank” href = "https://sph-fluid.hansonsun.repl.co">Simple Particle Hydrodynamics</a>(Interactive on a non-IOS smart phone)</li>
+			<li>and many others on my repl page.</li>
+
+		</ul>
+	</tr>
+	<tr>
+		Math Connect Website
+		<ul>
+			<li>A complete and fully functional website for my co-founded NPO, Math Connect.</li>
+			<li><a target=”_blank” href="https://mathconnect.ca/">Website Link</a></li>
+		</ul>
+	</tr>
+	<tr>
+		Interactive Periodic Table
+		<ul>
+			<li>A creative approach to a mundane chemistry 11 project.</li>
+			<li><a target=”_blank” href = "https://Periodic-Table.hansonsun.repl.co">Periodic Table</a></li>
+		</ul>
+	</tr>
+
+	<tr>
+		Exhibition of Javascript Art 
+		<ul>
+			<li>My first attempt at digital art</li>
+			<li>Javascript Interpretation of Process Compendium by Casey Reas</li>
+			<li>3q</li>
+		</ul>
+	</tr>
+
+</table>
+
+`
+
+contact = `
+Email: doctorhanson21@gmail.com
+Github: https://github.com/Hanson-Sun
+repl.it: https://replit.com/@HansonSun
+`
+
+mobile = `
+Oof, should check out this webiste on a computer sometime.
+`
+
+function pb(start, max, delay, flavtext, first = true) {
+	return new Promise((resolve) => {
+		let empty = "·";
+		let done = "#";
+		if (first) {
+			term.echo(flavtext + "[" + done.repeat(start) + empty.repeat(max - start) + "] " + (start / max * 100).toFixed(1) + "%")
+			first = false;
+		}
+		term.pause();
+		setTimeout(function () {
+			start++;
+
+			bar = flavtext + "[" + done.repeat(start) + empty.repeat(max - start) + "] " + (start / max * 100).toFixed(1) + "%";
+			term.update(term.last_index(), bar).resume();
+			if (start >= max) {
+				return resolve(true);
+
+			} else {
+				return resolve(pb(start, max, delay, flavtext, false));
+			}
+		}, delay);
+	});
+
+}
+
+
+
+
+var term = $('#content').terminal({
+	//this.echo("<a href = 'youtube.com'>I don't want to use a terminal</a>", {raw:true})
+
+	hello: function () {
+		this.echo('Hello, welcome to this terminal. Have fun looking around!');
+	},
+	help: function () {
+		this.echo(help, { raw: false });
+	},
+	about: function () {
+		this.echo(about, { raw: true });
+	},
+	skills: function () {
+		this.echo(skills, { raw: true });
+	},
+
+	projects: async function () {
+		// time = 42;
+		// size = 20;
+
+		// pb(0, size, time, "Loading Projects    ", projects).then(function () {
+		// 	term.update(-1, projects, { raw: true });
+		// })
+		this.echo( projects, { raw: true });
+
+	},
+
+	progress: function () {
+		pb(0, 20, 50, "Loading Data    ");
+	},
+	awards: function () {
+		this.echo(awards, { raw: true });
+	},
+	education: function () {
+		this.echo(education, { raw: true });
+	},
+	experience: function () {
+		this.echo(experience, { raw: true });
+	},
+	contact: function () {
+		this.echo(contact);
+	},
+	mobile: function () {
+
+	},
+	all: function () {
+		this.echo("About");
+		this.echo(about, { raw: true })
+		this.echo("Skills");
+		this.echo(skills, { raw: true });
+		this.echo("Experience");
+		this.echo(experience, { raw: true });
+		this.echo("Awards");
+		this.echo(awards, { raw: true });
+		this.echo("Projects");
+		this.echo(projects, { raw: true });
+		this.echo("Eductaion");
+		this.echo(education, { raw: true });
+		this.echo("Contact");
+		this.echo(contact);
+
+	},
+	download_cv: function () {
+
+	},
+	joke: function () {
+		this.pause();
+		setTimeout(() => {
+			this.echo("look in the mirror").resume();
+		}, 10);
+	},
+	bruh: function () {
+		this.echo("Bruh, you really gonna think bruh is a command");
+	}
+
+}, {
+		greetings: "",
+
+	});
+
+
+pb(0, 25, 45, "Loading Data    ", projects).then(function () {
+			term.update(-1, greet);
+});
