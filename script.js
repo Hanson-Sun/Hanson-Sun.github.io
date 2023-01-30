7dbb4cdb1bb4ec017dced0e1a1cf79347a9c98a6
Hanson Sun <doctorhanson21@gmail.com>
2023-01-30 00:15:29 -0800

    asd

--- script.js
+++ script.js
@@ -8,26 +8,25 @@ greet = `
 
 .....................................................................
  Welcome to my terminal, enter "help" for the documentation of all commands. 
- 
-`
+ `
 
 help = `
 +=============+===============================================+
-|  Function   |                      Use                      |
+|  Function   |                     Use                       |
 +=============+===============================================+
 | help        | displays a list of all the commands           |
 +-------------+-----------------------------------------------+
 | about       | A brief introduction of who I am              |
 +-------------+-----------------------------------------------+
-| skills      | My skills and abilities                       |
-+-------------+-----------------------------------------------+
 | experience  | my experiences and what I've accomplished     |
 +-------------+-----------------------------------------------+
 | projects    | a showcase of some of my programming projects |
 +-------------+-----------------------------------------------+
-| awards      | my achievements recognized by others          |
+| skills      | My skills and abilities                       |
 +-------------+-----------------------------------------------+
 | education   | a summary of my education journey             |
++-------------+-----------------------------------------------+
+| awards      | my achievements recognized by others          |
 +-------------+-----------------------------------------------+
 | contact     | my contact info                               |
 +-------------+-----------------------------------------------+
@@ -43,9 +42,7 @@ help = `
 +-------------+-----------------------------------------------+
 `
 about = `
-Hello! I'm Hanson Sun, an IB Diploma student at Richmond Secondary School. I am passionate
-about programming and technology. In my free time, I enjoy making fun
-programming projects and learning new skills. Other than that, I also have experience leading clubs and applying programming knowledge to real-world projects.
+Hi! I'm Hanson, a general science student with an interest in computer science. I am passionate about technology, education, and research. In my free time, I enjoy making fun programming projects and learning new skills. Other than that, I also have experience leading clubs and applying programming knowledge to real-world projects.
 <br>
 <br>
 Hobbies:
@@ -59,36 +56,35 @@ Hobbies:
 	</ul>
 `
 skills = `
-Programming
+	Programming
 
 	<table class = 'skills'>
 	<tr>
 		<td>+ HTML,CSS</td>
-		<td>▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░</td>
 	</tr>
 	<tr>
 		<td>+ Javascript</td>
-		<td>▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░ </td>
 	</tr>
 	<tr>
 		<td>+ Python</td>
-		<td>▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░</td>
+	</tr>
+	<tr>
+		<td>+ Java</td>
+	</tr>
+	<tr>
+		<td>+ Racket</td>
 	</tr>
 	<tr>
 		<td>+ LaTeX</td>
-		<td>▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░</td>
 	</tr>
 	<tr>
 		<td>+ Julia</td>
-		<td>▓▓▓▓▓▓▓▓▓▓▓▓░░░░░░░░</td>
 	</tr>
 	<tr>
 		<td>+ Git</td>
-		<td>▓▓▓▓▓▓▓▓▓▓▓░░░░░░░░░</td>
 	</tr>
 	<tr>
 		<td>+ C++</td>
-		<td>▓▓▓▓▓▓▓▓░░░░░░░░░░░░ </td>
 	</tr>
 	</table>
 Academics
@@ -96,19 +92,15 @@ Academics
 	<table class = 'skills'>
 	<tr>
 		<td>+ Mathematics</td>
-		<td>▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░</td>
 	</tr>
 	<tr>
 		<td>+ Physics</td>
-		<td>▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░ </td>
 	</tr>
 	<tr>
 		<td>+ Chemistry</td>
-		<td>▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░░</td>
 	</tr>
 	<tr>
 		<td>+ English</td>
-		<td>▓▓▓▓▓▓▓▓▓▓▓▓░░░░░░░░</td>
 	</tr>
  
 	</table>
@@ -116,28 +108,27 @@ General
 
 	<table class = 'skills'>
 	<tr>
-		<td>+ Chinese</td>
-		<td>▓▓▓▓▓▓▓▓▓▓░░░░░░░░░░</td>
+		<td>+ Mandarin</td>
+	</tr>
+	<tr>
+		<td>+ Cantonese</td>
 	</tr>
 	<tr>
 		<td>+ Communication</td>
-		<td>▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░</td>
 	</tr>
 	<tr>
 		<td>+ Leadership</td>
-		<td>▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░░░</td>
 	</tr>
 	<tr>
 		<td>+ Teamwork</td>
-		<td>▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░</td>
 	</tr>
-
 	</table>
 <br>
 `
 education = `
 	<ul class = "awards">
-		<li>Richmond Secondary School IB Diploma Student (hopefully grad soon)</li>
+		<li>Richmond Secondary School IB Diploma Student (2018 - 2022)</li>
+		<li>UBC Bachelor of Science (2022 - present)</li>
 		<li>More to come!</li>
 	</ul>
 
@@ -145,7 +136,18 @@ education = `
 experience = `
 	<ul class = "awards">
 		<li>
-			Co-founder of a non-profit math tutoring organization, Math Connect. (2020 - present)
+			UBC REX Participant (Undergraduate Research) (2022 - present)
+			<ul class = "awards">
+				<li>Conduct bio-computing research under the guidance of a graduate professor</li>
+				<li>Preparation to present at research conference MURC</li>
+				<li>Create cellular automata and ODE models with Morpheus and Jupyter Notebook</li>
+				<li>Research topic: A Step Forward in Understanding Skin Cancer: Modelling Keratinocyte Stem Cell
+				     Proliferation Regulated by p63 and c-MYC Proteins
+				</li>
+			</ul>
+		</li>
+		<li>
+			Co-founder of a non-profit math tutoring organization, Math Connect. (2020 - 2022)
 			<ul class = "awards">
 				<li>Planned, designed, and launched a free NPO, Math Connect</li>
 				<li>Interviewed high school students as potential tutors</li>
@@ -154,7 +156,7 @@ experience = `
 			</ul>
 		</li>
 		<li>
-			Co-founder of the RHS Programming Club (2020 - present)
+			Co-founder of the RHS Programming Club (2020 - 2022)
 			<ul class = "awards">
 				<li>Created interactive programming curriculum and lessons every week</li>
 				<li>Teaches creative programming lessons every week</li>
@@ -163,14 +165,14 @@ experience = `
 			</ul>
 		</li>
 		<li>
-			Co-president of the RHS Math Club (2019 - present)
+			President of the RHS Math Club (2020 - 2022)
 			<ul class = "awards">
 				<li>Organized large scale math competitions such as the COMC, CSMC/CIMC, and Euclid</li>
 				<li>Managed finances from student payments</li>
 			</ul>
 		</li>
 		<li>
-			Member of the RHS Science Team (2018 - present)
+			Member of the RHS Science Team (2018 - 2022)
 			<ul class = "awards">
 				<li>Competes in various different science competitions</li>
 				<li>Plan and design pe-build events</li>
@@ -188,6 +190,7 @@ experience = `
 
 awards = `
 	<ul class = "awards">
+		<li>Governor General's Bronze Medal</li>
 		<li>1st place in district for 2021 Waterloo's Sir Isaac Newton Physics Exam</li>
 		<li>82nd place in Canada for 2021 Waterloo's Avogadro Contest</li>
 		<li>9th place during 2021 UBC Physics Olympics, placing 3rd for the Speed of Sound Event (the event I was mainly responsible for)</li>
@@ -202,7 +205,7 @@ awards = `
 
 projects = `
 
-A list of notable cool things I've made, some for fun, some a bit less fun
+A list of notable cool things I've made, some for fun, some more serious
 <br><br>
 <table>
 	<tr>
@@ -226,7 +229,14 @@ A list of notable cool things I've made, some for fun, some a bit less fun
 		<ul>
 			<li>A mathematical analysis on visualizing 4D polygons with a threejs javaScript implementation</li>
 			<li><a target = "_blank"href="https://hanson-sun.github.io/4d-rendering/">Demo Link</a></li>
-			<li><a target = "_blank"href="https://github.com/Hanson-Sun/4d-rendering/blob/main/math_ia.pdf">Math paper</a> (work in progress)</li>
+			<li><a target = "_blank"href="https://github.com/Hanson-Sun/4d-rendering/blob/main/math_ia.pdf">Math paper</a></li>
+		</ul>
+	</tr>
+	<tr>
+		Drawing with Sound
+		<ul>
+			<li>Rendering bitmap images on a digital oscilloscope with Fourier Transform and Image Processing</li>
+			<li><a target = "_blank"href="https://github.com/Hanson-Sun/Drawing-With-Sound">Source Code and Demo</a></li>
 		</ul>
 	</tr>
 	<tr>
@@ -254,8 +264,15 @@ A list of notable cool things I've made, some for fun, some a bit less fun
 	<tr>
 		Interactive Periodic Table
 		<ul>
-			<li>A creative approach to a mundane chemistry 11 project.</li>
+			<li>A creative approach to visualize periodic trends</li>
 			<li><a target = "_blank" href = "https://Periodic-Table.hansonsun.repl.co">Periodic Table</a></li>
+		</ul>
+	</tr>
+	<tr>
+	CPSC (a Java Flashcards app)
+		<ul>
+			<li>A unique desktop java flashcard application, really helps me study bio!</li>
+			<li><a target = "_blank" href = "">WORK IN PROGRESS</a></li>
 		</ul>
 	</tr>
 
@@ -284,160 +301,158 @@ Oof, should check out this website on a computer sometime.
 `
 
 function pb(start, max, delay, flavtext, first = true) {
-    return new Promise((resolve) => {
-        let empty = "·";
-        let done = "#";
-        if (first) {
-            term.echo(flavtext + "[" + done.repeat(start) + empty.repeat(max - start) + "] " + (start / max * 100).toFixed(1) + "%")
-            first = false;
-        }
-        term.pause();
-        setTimeout(function() {
-            start++;
+	return new Promise((resolve) => {
+		let empty = "·";
+		let done = "#";
+		if (first) {
+			term.echo(flavtext + "[" + done.repeat(start) + empty.repeat(max - start) + "] " + (start / max * 100).toFixed(1) + "%");
+			first = false;
+		}
+		term.pause();
+		setTimeout(function () {
+			start++;
 
-            bar = flavtext + "[" + done.repeat(start) + empty.repeat(max - start) + "] " + (start / max * 100).toFixed(1) + "%";
-            term.update(term.last_index(), bar).resume();
-            if (start >= max) {
-                return resolve(true);
+			bar = flavtext + "[" + done.repeat(start) + empty.repeat(max - start) + "] " + (start / max * 100).toFixed(1) + "%";
+			term.update(term.last_index(), bar).resume();
+			if (start >= max) {
+				return resolve(true);
 
-            } else {
-                return resolve(pb(start, max, delay, flavtext, false));
-            }
-        }, delay);
-    });
+			} else {
+				return resolve(pb(start, max, delay, flavtext, false));
+			}
+		}, delay);
+	});
 
 }
 
 function fetchURL(url) {
-    return new Promise((resolve) => {
-        let response = fetch(url, { credentials: 'omit' }).then(res => {
-            if (!res.ok) {
-                throw new Error('Network response was not OK');
-            }
-            return res.json();
-        }).then(data => {
-            return resolve(data.joke)
-        });
-    })
+	return new Promise((resolve) => {
+		let response = fetch(url, { credentials: 'omit' }).then(res => {
+			if (!res.ok) {
+				throw new Error('Network response was not OK');
+			}
+			return res.json();
+		}).then(data => {
+			return resolve(data.joke)
+		});
+	})
 }
 
 
 
 
 var term = $('#content').terminal({
-    //this.echo("<a href = 'youtube.com'>I don't want to use a terminal</a>", {raw:true})
+	//this.echo("<a href = 'youtube.com'>I don't want to use a terminal</a>", {raw:true})
 
-    hello: function() {
-        this.echo('Hello, welcome to this terminal. Have fun looking around!');
-    },
-    help: function() {
-        this.echo(help, { raw: false });
-    },
-    about: function() {
-        this.echo(about, { raw: true });
-    },
-    skills: function() {
-        this.echo(skills, { raw: true });
-    },
+	hello: function () {
+		this.echo('Hello, welcome to this terminal. Have fun looking around!');
+	},
+	help: function () {
+		this.echo(help, { raw: false });
+	},
+	about: function () {
+		this.echo(about, { raw: true });
+		this.echo(asciiHanson, { raw: true });
+	},
+	skills: function () {
+		this.echo(skills, { raw: true });
+	},
 
-    projects: async function() {
-        // time = 42;
-        // size = 20;
+	projects: async function () {
+		// time = 42;
+		// size = 20;
 
-        // pb(0, size, time, "Loading Projects    ", projects).then(function () {
-        // 	term.update(-1, projects, { raw: true });
-        // })
-        this.echo(projects, { raw: true });
+		// pb(0, size, time, "Loading Projects    ", projects).then(function () {
+		// 	term.update(-1, projects, { raw: true });
+		// })
+		this.echo(projects, { raw: true });
 
-    },
+	},
 
-    progress: function() {
-        pb(0, 20, 50, "Loading Data    ");
-    },
-    awards: function() {
-        this.echo(awards, { raw: true });
-    },
-    education: function() {
-        this.echo(education, { raw: true });
-    },
-    experience: function() {
-        this.echo(experience, { raw: true });
-    },
-    contact: function() {
-        this.echo(contact, { raw: true });
-    },
-    mobile: function() {
+	progress: function () {
+		pb(0, 20, 50, "Loading Data    ");
+	},
+	awards: function () {
+		this.echo(awards, { raw: true });
+	},
+	education: function () {
+		this.echo(education, { raw: true });
+	},
+	experience: function () {
+		this.echo(experience, { raw: true });
+	},
+	contact: function () {
+		this.echo(contact, { raw: true });
+	},
+	mobile: function () {
 
-    },
-    all: function() {
-        this.echo("<h1>About</h1><br>", { raw: true });
-        this.echo(about, { raw: true })
-        this.echo("<h1>Skills</h1><br>", { raw: true });
-        this.echo(skills, { raw: true });
-        this.echo("<h1>Experience</h1>", { raw: true });
-        this.echo(experience, { raw: true });
-        this.echo("<h1>Awards</h1>", { raw: true });
-        this.echo(awards, { raw: true });
-        this.echo("<h1>Projects</h1><br>", { raw: true });
-        this.echo(projects, { raw: true });
-        this.echo("<h1>Education</h1>", { raw: true });
-        this.echo(education, { raw: true });
-        this.echo("<h1>Contact</h1>", { raw: true });
-        this.echo(contact, { raw: true });
+	},
+	all: function () {
+		this.echo("<h1>About</h1><br>", { raw: true });
+		this.echo(about, { raw: true })
+		this.echo("<h1>Skills</h1><br>", { raw: true });
+		this.echo(skills, { raw: true });
+		this.echo("<h1>Experience</h1>", { raw: true });
+		this.echo(experience, { raw: true });
+		this.echo("<h1>Awards</h1>", { raw: true });
+		this.echo(awards, { raw: true });
+		this.echo("<h1>Projects</h1><br>", { raw: true });
+		this.echo(projects, { raw: true });
+		this.echo("<h1>Education</h1>", { raw: true });
+		this.echo(education, { raw: true });
+		this.echo("<h1>Contact</h1>", { raw: true });
+		this.echo(contact, { raw: true });
 
-    },
-    download_cv: function() {
-        this.echo("Opening CV. . .")
-        window.open("resume.pdf", '_blank').focus();
-    },
-    joke: async function() {
-        if (firstJoke) {
-            this.echo("These jokes are from a javscript joke API and I am NOT responsible for any questionable content that may appear. \n")
-            firstJoke = false;
-        }
-        this.pause();
-        const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
-        let response = await fetch(url).then(res => {
-            return res.json();
-        }).then(data => {
-            if (data.joke) { return data.joke } else {
-                return data.setup + "\n" + data.delivery
-            }
+	},
+	download_cv: function () {
+		this.echo("Opening CV. . .")
+		window.open("resume-coop.pdf", '_blank').focus();
+	},
+	joke: async function () {
+		if (firstJoke) {
+			this.echo("These jokes are from a javscript joke API and I am NOT responsible for any questionable content that may appear. \n")
+			firstJoke = false;
+		}
+		this.pause();
+		const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
+		let response = await fetch(url).then(res => {
+			return res.json();
+		}).then(data => {
+			if (data.joke) { return data.joke } else {
+				return data.setup + "\n" + data.delivery
+			}
+		});
+		console.log(response)
+		this.echo(response).resume();
+		// response = fetchURL("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit");
+		// console.log(response)
+		// this.echo(response);
+	},
+	bored: async function () {
+		this.pause();
+		const url = "https://www.boredapi.com/api/activity";
+		let response = await fetch(url).then(res => {
+			return res.json();
+		}).then(data => {
+			if (data.activity) { return data.activity }
+		});
+		console.log(response)
+		this.echo(response).resume();
 
+		// response = fetchURL("https://www.boredapi.com/api/activity");
 
-        });
-        console.log(response)
-        this.echo(response).resume();
-        // response = fetchURL("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit");
-        // console.log(response)
-        // this.echo(response);
-    },
-    bored: async function() {
-        this.pause();
-        const url = "https://www.boredapi.com/api/activity";
-        let response = await fetch(url).then(res => {
-            return res.json();
-        }).then(data => {
-            if (data.activity) { return data.activity }
-        });
-        console.log(response)
-        this.echo(response).resume();
-
-        // response = fetchURL("https://www.boredapi.com/api/activity");
-
-        // console.log(response)
-        // this.echo(response);
-    },
-    bruh: function() {
-        this.echo("Bruh, you really gonna think bruh is a command");
-    }
+		// console.log(response)
+		// this.echo(response);
+	},
+	bruh: function () {
+		this.echo("Bruh, you really gonna think bruh is a command");
+	}
 
 }, {
-    greetings: "",
-
+	greetings: ""
 });
 
 
-pb(0, 25, 45, "Loading Data    ", projects).then(function() {
-    term.update(-1, greet);
+pb(0, 25, 45, "Loading Data    ", projects).then(function () {
+	term.update(-1, greet);
 });
